ebe6c0f1a3106a1b8ba5f74e12bf3e2932e0856c
DrBake <109722467+DrBake@users.noreply.github.com>
2024-01-04 16:40:43 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Drake Unrelated - The Unofficial Website of Drake ",
+  title: "Drake Relayted - The Unofficial Website of Drake ",
   description: "A quickly put together copy of drakerelated.com",
 };
 
